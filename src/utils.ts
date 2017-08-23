49ec5cdcc53effd0571bb31838c2498e874ac53c
Preet <preet@hivepoint.com>
2017-08-23 15:07:38 -0700

    added pem key generation

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,23 @@
 const secp256k1 = require('secp256k1');
 const ethereumUtils = require('ethereumjs-util');
+const KeyEncoder = require('key-encoder');
 
 export class Utils {
-  generateAddress(pkey: string): string[] {
+  generateAddress(pkey: string): any {
     const privateKey = new Uint8Array(new Buffer(pkey, 'base64'));
     const publicKey = secp256k1.publicKeyCreate(new Buffer(privateKey)) as Uint8Array;
     const ethPublic = ethereumUtils.importPublic(new Buffer(publicKey)) as Uint8Array;
-    const ethAddress = ethereumUtils.pubToAddress(ethPublic, false) as Uint8Array;
-    return [
-      new Buffer(publicKey).toString('base64'),
-      new Buffer(ethPublic).toString('base64'),
-      new Buffer(ethAddress).toString('base64')
-    ];
+    const address = ethereumUtils.pubToAddress(ethPublic, false) as Uint8Array;
+    const keyEncoder = new KeyEncoder('secp256k1');
+    const privateKeyPem = keyEncoder.encodePrivate(new Buffer(privateKey).toString('hex'), 'raw', 'pem');
+    const publicKeyPem = keyEncoder.encodePublic(new Buffer(publicKey).toString('hex'), 'raw', 'pem');
+    return {
+      privateKeyPem: privateKeyPem,
+      publicKey: (new Buffer(publicKey).toString('base64')),
+      publicKeyPem: publicKeyPem,
+      ethPublic: (new Buffer(ethPublic).toString('base64')),
+      address: (new Buffer(address).toString('base64')),
+      ethAddress: '0x' + new Buffer(address).toString('hex')
+    };
   }
 }
